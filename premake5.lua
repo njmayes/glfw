45f44d333d370e3bb58e88b8b9eaeb3eae0d260f
Nathan Mayes <nmayes@innov8gaming.com>
2025-05-16 09:47:26 +0100

    Fix premake file

--- premake5.lua
+++ premake5.lua
@@ -10,25 +10,19 @@ project "glfw"
     { 
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/internal.h",
-		"src/platform.h",
-		"src/mappings.h",
 		"src/context.c",
+		"src/platform.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
-		"src/platform.c",
 		"src/vulkan.c",
 		"src/window.c",
-		"src/egl_context.c",
-		"src/osmesa_context.c",
-		"src/null_platform.h",
-		"src/null_joystick.h",
-		"src/null_init.c",
-
-		"src/null_monitor.c",
-		"src/null_window.c",
-		"src/null_joystick.c",
+		"src/egl_context.c", 
+		"src/osmesa_context.c", 
+		"src/null_init.c", 
+		"src/null_monitor.c", 
+		"src/null_window.c", 
+		"src/null_joystick.c"
     }
 	
 	filter "files:**.h"
@@ -41,17 +35,15 @@ project "glfw"
 
 		files
 		{
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/posix_module.c",
+			"src/xkb_unicode.h",
 			"src/x11_init.c",
 			"src/x11_monitor.c",
 			"src/x11_window.c",
 			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/posix_poll.c",
 			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
 			"src/linux_joystick.c"
 		}
 
@@ -65,18 +57,14 @@ project "glfw"
 
 		files
 		{
-			"src/win32_init.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
 			"src/win32_module.c",
+			"src/win32_init.c",
 			"src/win32_joystick.c",
 			"src/win32_monitor.c",
-			"src/win32_time.h",
-			"src/win32_time.c",
-			"src/win32_thread.h",
-			"src/win32_thread.c",
 			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
+			"src/wgl_context.c"
 		}
 
 		defines 
